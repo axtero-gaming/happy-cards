810ad5584ca34e9919a2fe6e781fc62ad3becdbb
Andrey <geass1000@gmail.com>
2023-06-16 01:58:10 +0300

    feat(docs): add code-solver build

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -1,7 +1,7 @@
 "use strict";
 (self["webpackChunkclient"] = self["webpackChunkclient"] || []).push([[429],{
 
-/***/ 4924:
+/***/ 4946:
 /*!***********************************************!*\
   !*** ./node_modules/zone.js/fesm2015/zone.js ***!
   \***********************************************/
@@ -2734,7 +2734,7 @@ Zone.__load_patch('PromiseRejectionEvent', (global, Zone) => {
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__(4924));
+/******/ var __webpack_exports__ = (__webpack_exec__(4946));
 /******/ }
 ]);
 //# sourceMappingURL=polyfills.js.map
